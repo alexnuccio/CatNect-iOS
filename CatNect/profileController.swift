fff7bfb4f6d463999cd522625b3b335f340558dd
Weicheng Yu <weichengyu@email.arizona.edu>
2015-10-01 20:17:29 -0700

    Some initial work for profile page

---
+++ CatNect/profileController.swift
@@ -0,0 +1,33 @@
+//
+//  profileController.swift
+//  CatNect
+//
+//  Created by Weicheng on 9/30/15.
+//  Copyright © 2015 Alex Nuccio. All rights reserved.
+//
+
+import Foundation
+
+class profileController: ViewController {
+    
+    
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var phoneLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        populateLabels()
+    }
+    
+    func populateLabels() {
+        dispatch_async(dispatch_get_main_queue()) {
+            
+            
+            
+            
+        }
+
+    }
+}
