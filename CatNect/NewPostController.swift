2913c87c0293299f8b4f551cc1b7fb9a06b9c41c
alexnuccio <alexnuccio@email.arizona.edu>
2015-10-07 18:07:24 -0700

    new post functionality is WORKING

--- CatNect/NewPostController.swift
+++ CatNect/NewPostController.swift
@@ -10,6 +10,54 @@ import Foundation
 
 class NewPostController: UIViewController {
     
+    @IBOutlet weak var titleText: UITextField!
+    
+    
+    @IBOutlet weak var locationText: UITextField!
+    
+    
+    @IBOutlet weak var descriptionText: UITextField!
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func submitClicked(sender: UIButton) {
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8000/newPost")!)
+        let session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        var date: String = "\((datePicker?.date)!)"
+        date = date.substringWithRange(Range<String.Index>(start: date.startIndex, end: date.startIndex.advancedBy(19)))
+        let location = locationText?.text!
+        //let title = titleText?.text
+        let bodyText = descriptionText?.text!
+        let username = variables.currentUser["username"]!
+        //TODO:add title to post
+        let string: NSString = "username=\(username)&date=\(date)&location=\(location)&body=\(bodyText)"
+        let body = string.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+        request.HTTPBody = body
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            if let httpResponse = response as? NSHTTPURLResponse {
+                print(httpResponse.statusCode)
+                
+                if httpResponse.statusCode == 200 {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.displayAlertWithTitle("Successfully Posted", message: "Successfully created a new event post")
+                        self.performSegueWithIdentifier("postReturnSegue", sender: self)
+                    }
+                    
+                } else {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.displayAlertWithTitle("Cancelled Post", message: "Cancelled new post")
+                        return
+                    }
+                }
+                
+            }
+            
+        })
+        task.resume()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +68,11 @@ class NewPostController: UIViewController {
         super.viewDidAppear(animated)
         
     }
+    
+    func displayAlertWithTitle(tite: String, message: String) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        controller.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(controller, animated: true, completion: nil)
+    }
+
 }
